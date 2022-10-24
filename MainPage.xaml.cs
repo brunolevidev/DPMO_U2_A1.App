a7934b57fb35da0d7a1cb6469c95d828d23568ae
Bruno Velazquez <contacto.bruno.velazquez@gmail.com>
2022-10-23 21:18:34 -0500

    implemented logic business

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,5 +6,33 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void realizarConversion(object sender, EventArgs args)
+	{
+		double x = 0;
+        int cantidadAConvertir = int.Parse(cantidad.Text);
+        switch (divisa.SelectedIndex)
+		{
+			case 0:
+				x = cantidadAConvertir * 19.92;
+				break;
+			case 1:
+				x = cantidadAConvertir * 19.64;
+                break;
+			case 2:
+                x = cantidadAConvertir * 22.59;
+                break;
+			case 3:
+                x = cantidadAConvertir * 0.13;
+                break;
+			case 4:
+                x = cantidadAConvertir * 0.32;
+                break;
+			default:
+				x = 2 + 1;
+				break;
+		}
+		respuesta.Text = $"{cantidad.Text} equivale a {x:0.##} {divisa.SelectedItem}";
+	}
 }
 
